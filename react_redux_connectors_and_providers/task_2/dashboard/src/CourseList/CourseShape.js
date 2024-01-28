18c27aecfdd4d0ecaa79dd53b87968cd828baf06
faroukbmiled <fcfaroukfc@gmail.com>
2024-01-28 16:10:57 +0100

    rm

--- react_redux_connectors_and_providers/task_2/dashboard/src/CourseList/CourseShape.js
+++
@@ -1,9 +0,0 @@
-import propTypes from "prop-types";
-
-const CourseShape = propTypes.shape({
-  id: propTypes.number,
-  name: propTypes.string,
-  credit: propTypes.number,
-});
-
-export default CourseShape;
